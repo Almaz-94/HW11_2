2d96b31c5fe050523866ab246a9e3534aca87335
Almaz <almaz28-10@mail.ru>
2023-09-23 13:14:18 +0300

    added docstrings to str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,10 @@
 def upper(string):
+    '''
+    capitalise everything
+    '''
     return string.upper()
+def upper_first(string):
+    '''
+    capitalise every word in a string
+    '''
+    return ' '.join([word.capitalize() for word in string.split()])
